652c8e7fb3f009de0945f84ae1df290bf0a34907
Esteban <estebonch0611@gmail.com>
2022-02-18 14:52:22 -0600

    AnswersAPP

---
+++ AnswersAPP_EstebanVasquez/AnswersAPP_EstebanVasquez/ViewModels/LoginViewModel.cs
@@ -0,0 +1,24 @@
+﻿using AnswersAPP_EstebanVasquez.Views;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace AnswersAPP_EstebanVasquez.ViewModels
+{
+    public class LoginViewModel : BaseViewModel
+    {
+        public Command LoginCommand { get; }
+
+        public LoginViewModel()
+        {
+            LoginCommand = new Command(OnLoginClicked);
+        }
+
+        private async void OnLoginClicked(object obj)
+        {
+            // Prefixing with `//` switches to a different navigation stack instead of pushing to the active one
+            await Shell.Current.GoToAsync($"//{nameof(AboutPage)}");
+        }
+    }
+}
---
+++ AnswersAPP_EstebanVasquez/AnswersAPP_EstebanVasquez/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using AnswersAPP_EstebanVasquez.Models;
+using AnswersAPP_EstebanVasquez.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AnswersAPP_EstebanVasquez.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
