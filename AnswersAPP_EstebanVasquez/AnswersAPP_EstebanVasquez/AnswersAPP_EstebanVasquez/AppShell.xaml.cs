70ed13e384d522fb4e58da661fc6e9b8bc9dd552
Esteban <estebonch0611@gmail.com>
2022-03-02 16:01:42 -0600

    APP

---
+++ AnswersAPP_EstebanVasquez/AnswersAPP_EstebanVasquez/AnswersAPP_EstebanVasquez/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using AnswersAPP_EstebanVasquez.ViewModels;
+using AnswersAPP_EstebanVasquez.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace AnswersAPP_EstebanVasquez
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
